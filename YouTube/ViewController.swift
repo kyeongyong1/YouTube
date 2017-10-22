72d3e890779027e711139d2575d0b10c7cec8546
Lee <kyeongyong1@github.com>
2017-10-23 00:24:07 +0900

    Initial Commit

---
+++ YouTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Lee on 2017. 10. 23..
+//  Copyright © 2017년 LEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
